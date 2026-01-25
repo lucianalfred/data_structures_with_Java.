aba4962582de6fd54a58044306ec06de9f71925f
Lucian Alfred <lucianalfred60@gmailcom>
2026-01-25 17:17:04 +0100

    add and remove

--- src/ed/Vector.java
+++ src/ed/Vector.java
@@ -4,36 +4,93 @@ package ed;
 public class Vector {
 	
 	private Aluno[] alunos = new Aluno[100];
-	
+	private  int totalDeAlunos = 0; 
 	
 	public void adiciona(Aluno aluno) {
 	
 		for (int i = 0; i < this.alunos.length; i++) {
 			if (this.alunos[i] == null) {
 				this.alunos[i] = aluno;
+				this.totalDeAlunos += 1;
 				break;
 			}
 		}
 	}
 	
+	public int tamanho() {
+		return this.totalDeAlunos;
+	}
+	
+	private boolean posicaoValida(int posicao) {
+		return posicao >= 0 && posicao <= this.totalDeAlunos;
+	}
+	
 	public void adiciona(int posicao, Aluno aluno) {
 		
+		if (this.posicaoValida(posicao)) {
+			throw new IllegalArgumentException("Posicao invalida!");
+		}
+		
+		for (int i = this.totalDeAlunos - 1; i >= posicao; i--) {
+			this.alunos[i + 1] = this.alunos[i];
+		}
+		
+		this.alunos[posicao] = aluno;
+		this.totalDeAlunos++;
+	}
+	
+	private boolean posicaoOcupada(int posicao){
+		return posicao >= 0 && posicao < this.totalDeAlunos;
 	}
 	
 	public Aluno pega(int posicao) {
-		return null;
+		if (!this.posicaoOcupada(posicao)) {
+			throw new IllegalArgumentException("Posicao Invalida");
+		}
+		
+		return this.alunos[posicao];
 	}
 	
 	public void remove(int posicao) {
+		if (this.posicaoOcupada(posicao)) {
+			throw new IllegalArgumentException("Posicao invalida");
+		}
 		
+		for (int i = posicao; i < this.totalDeAlunos - 1; i++) {
+			this.alunos[i] = this.alunos[i + 1];
+		}
+		this.totalDeAlunos--;
 	}
 	
 	public boolean contem(Aluno aluno) {
+		
+		for (int i = 0; i < this.totalDeAlunos; i++) {
+			if (aluno.equals(this.alunos[i])) {
+				return true;
+			}
+		}
 		return false;
-	}
+	}	
 	
-	public int tamanho() {
-		return 0;
+	public String toString() {
+		if (this.totalDeAlunos == 0) {
+			return "[]";
+		}
+		
+		StringBuilder builder = new StringBuilder();
+		
+		builder.append("[");
+		
+		for (int i = 0; i < this.totalDeAlunos - 1; i++)
+		{
+			builder.append(this.alunos[i]);
+			builder.append(", ");
+		}
+		
+		builder.append(this.alunos[this.totalDeAlunos - 1]);
+		
+		
+		return super.toString();
 	}
 	
 }
